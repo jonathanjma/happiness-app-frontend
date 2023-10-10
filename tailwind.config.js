18fcbd75a1fdc7c2fb1e4a959a6e4b473d541a3e
Jonathan Ma <jonathanjma03@gmail.com>
2023-10-10 12:23:14 -0400

    update default typography and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    fontSize: {
+      /* default design font sizes and line heights */
+      sm: ["0.875rem", "1.5rem"] /* label */,
+      base: ["1rem", "1.5rem"] /* body */,
+      lg: ["1.25rem", "1.5rem"] /* h5 */,
+      xl: ["1.5rem", "2rem"] /* h4 */,
+      "2xl": ["1.75rem", "2.25rem"] /* h3 */,
+      "3xl": ["2rem", "2.25rem"] /* h2 */,
+      "4xl": ["2.25rem", "2.75rem"] /* h1 */,
+    },
+    colors: {
+      black: "#212121",
+      dark_gray: "#575F68",
+      light_gray: "#808080",
+      light_yellow: "#F6F0E0",
+      medium_yellow: "#F6E2AE",
+      yellow: "#F0CF78",
+    },
   },
   content: ["./src/**/*.{js,ts,jsx,tsx}", "node_modules/preline/dist/*.js"],
   plugins: [require("preline/plugin")],
