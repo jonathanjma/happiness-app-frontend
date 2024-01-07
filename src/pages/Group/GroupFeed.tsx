0c2505b88b47126d95d113ebe82ce25d792ea5fa
Jonathan Ma <jonathanjma03@gmail.com>
2024-01-06 20:39:08 -0800

    more refactoring (having wierd react query issues though)

--- src/pages/Group/GroupFeed.tsx
+++ src/pages/Group/GroupFeed.tsx
@@ -35,6 +35,11 @@ export default function GroupFeed({ groupData }: { groupData: Group }) {
   };
 
   const fetcher = async (page: number, unreadReq: boolean) => {
+    const pageData = unreadReq ? unreadQuery.data : feedQuery.data;
+
+    // if (pageData) console.log(pageData.pages[page - 1]);
+    console.log(page, unreadReq);
+
     const res = await api.get<Happiness[]>(
       `/group/${groupData.id}/happiness/${unreadReq ? "unread" : "count"}`,
       {
@@ -42,26 +47,22 @@ export default function GroupFeed({ groupData }: { groupData: Group }) {
       },
     );
 
-    if (!unreadReq) {
-      res.data.sort(
-        (a, b) =>
-          new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime(),
-      );
-    }
-
     const newPageElements = [];
-    const pageData = unreadReq ? unreadData : feedData;
     const lastPage = pageData
       ? pageData.pages[pageData.pages.length - 1].data
       : undefined;
     // get last entry on last page
+    console.log(pageData);
     let prevDate = lastPage ? lastPage[lastPage.length - 1].timestamp : "";
 
     // add entries from last page to feed and add date seperator between entry cards with different dates
     for (let entry of res.data) {
       if (prevDate !== entry.timestamp) {
         newPageElements.push(
-          <h5 key={entry.timestamp} className="mb-4 text-gray-400">
+          <h5
+            key={entry.timestamp + " " + unreadReq}
+            className="mb-4 text-gray-400"
+          >
             {dateOrTodayYesterday(
               entry.timestamp,
               dateFromStr(entry.timestamp).toLocaleDateString("en-us", {
@@ -75,7 +76,7 @@ export default function GroupFeed({ groupData }: { groupData: Group }) {
       }
       newPageElements.push(
         <FeedCard
-          key={entry.id}
+          key={entry.id * (unreadReq ? -1 : 1)}
           data={entry}
           isNew={unreadReq}
           onClick={() => setSelectedEntry(entry)}
@@ -95,80 +96,67 @@ export default function GroupFeed({ groupData }: { groupData: Group }) {
   };
 
   // infinite query for fetching unread entries
-  const {
-    isLoading: unreadIsLoading,
-    data: unreadData,
-    isError: unreadIsError,
-    fetchNextPage: unreadFetchNextPage,
-    hasNextPage: unreadHasNextPage,
-  } = useInfiniteQuery<HappinessPagination>({
+  const unreadQuery = useInfiniteQuery<HappinessPagination>({
     queryKey: [QueryKeys.FETCH_GROUP_HAPPINESS_UNREAD, groupData.id],
     queryFn: ({ pageParam = 1 }) => fetcher(pageParam, true),
     getNextPageParam: getNextPageParam,
   });
 
-  // infinite query for fetching entries
-  const {
-    isLoading: feedIsLoading,
-    data: feedData,
-    isError: feedIsError,
-    fetchNextPage: feedFetchNextPage,
-    hasNextPage: feedHasNextPage,
-  } = useInfiniteQuery<HappinessPagination>({
+  // infinite query for fetching regular entries
+  const feedQuery = useInfiniteQuery<HappinessPagination>({
     queryKey: [QueryKeys.FETCH_GROUP_HAPPINESS, groupData.id],
     queryFn: ({ pageParam = 1 }) => fetcher(pageParam, false),
     getNextPageParam: getNextPageParam,
-    // loads first page of regular feed on initial load
-    // (add unreadHasNextPage !== undefined condition to disable this)
-    enabled: !unreadHasNextPage,
   });
 
   // load more entries when bottom reached
   useEffect(() => {
-    if (unreadInView && unreadHasNextPage) unreadFetchNextPage();
+    if (unreadInView && unreadQuery.hasNextPage) unreadQuery.fetchNextPage();
   }, [unreadInView]);
 
   useEffect(() => {
-    if (bottomInView && feedHasNextPage) feedFetchNextPage();
+    if (bottomInView && feedQuery.hasNextPage) feedQuery.fetchNextPage();
   }, [bottomInView]);
 
   return (
     <div className="mx-8">
       {/* Unread feed */}
-      {unreadElements.length > 0 && <h5 className="mb-4">Unread</h5>}
-      {unreadIsLoading ? (
+      {unreadQuery.isLoading ? (
         <Spinner />
       ) : (
         <>
-          {unreadIsError ? (
+          {unreadQuery.isError ? (
             <h5 className="text-gray-400">Error: Could not load entries.</h5>
           ) : (
             <div>
+              {unreadElements.length > 0 && <h5 className="mb-4">Unread</h5>}
               <>{unreadElements}</>
               <div ref={unreadRef} className="m-3">
-                {unreadHasNextPage && <Spinner text="Loading entries..." />}
+                {unreadQuery.hasNextPage && (
+                  <Spinner text="Loading entries..." />
+                )}
               </div>
             </div>
           )}
         </>
       )}
       {/* Don't show regular feed until unread feed has been consumed */}
-      {unreadHasNextPage !== undefined && !unreadHasNextPage && (
+      {unreadQuery.hasNextPage !== undefined && !unreadQuery.hasNextPage && (
         <>
-          <h5 className="my-4">Feed</h5>
-          {feedIsLoading ? (
+          {feedQuery.isLoading ? (
             <Spinner />
           ) : (
             <>
-              {feedIsError ? (
+              {feedQuery.isError ? (
                 <h5 className="text-gray-400">
                   Error: Could not load entries.
                 </h5>
               ) : (
                 <div>
+                  <h5 className="my-4">Feed</h5>
                   <>{feedElements}</>
                   <div ref={bottomRef} className="m-3">
-                    {feedHasNextPage ? (
+                    {feedQuery.hasNextPage ? (
                       <Spinner text="Loading entries..." />
                     ) : (
                       <p className="">No more entries!</p>
