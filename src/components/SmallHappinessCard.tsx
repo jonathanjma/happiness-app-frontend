9a06f381d3aa0ca5d870def3acca9c6a6dafcb4d
Jonathan Ma <jonathanjma03@gmail.com>
2024-01-22 16:45:19 -0500

    fix styling

--- src/components/SmallHappinessCard.tsx
+++ src/components/SmallHappinessCard.tsx
@@ -15,7 +15,13 @@ interface Action {
  * @param {Happiness} happiness - An object of type `Happiness` which contains the details of a happiness event. It has properties like `value`, `timestamp`, and `author` which contains the `username`.
  * @param {Action[]} actions - An optional array of `Action` objects. This will create buttons as seen in design
  */
-export default function SmallHappinessCard({ happiness, actions = [] }: { happiness: Happiness, actions?: Action[]; }) {
+export default function SmallHappinessCard({
+  happiness,
+  actions = [],
+}: {
+  happiness: Happiness;
+  actions?: Action[];
+}) {
   const today = new Date();
   today.setHours(0, 0, 0, 0);
 
@@ -23,36 +29,39 @@ export default function SmallHappinessCard({ happiness, actions = [] }: { happin
 
   return (
     <Card className="p-4">
-      <Row className="items-center">
-        <div className="bg-light_yellow rounded-[4px] w-11 h-11 flex items-center justify-center">
-          <h4 className="text-secondary">{happiness.value}</h4>
+      <Row className="items-center gap-x-2">
+        <div className="flex h-11 w-11 items-center justify-center rounded-[4px] bg-light_yellow">
+          <h4 className="text-secondary">{happiness.value.toFixed(1)}</h4>
         </div>
         <div className="w-1" />
         <Column>
           <p className="text-gray-600">{happiness.author.username}</p>
-          <label className="text-gray-400">{
-            happinessDate < today ?
-              happinessDate.toLocaleDateString("en-us", {
-                month: "long",
-                day: "numeric",
-                year: "numeric"
-              }) : "Today"
-          }</label>
+          <label className="font-normal text-gray-400">
+            {happinessDate < today
+              ? happinessDate.toLocaleDateString("en-us", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })
+              : "Today"}
+          </label>
         </Column>
         <div className="flex flex-grow" />
         <Row className="gap-3">
-          {actions.map((action) =>
+          {actions.map((action) => (
             <div
-              className="px-3 py-1 bg-gray-50 rounded-[4px] hover:cursor-pointer"
+              className="rounded-[4px] bg-gray-50 px-3 py-1 hover:cursor-pointer"
               onClick={action.onClick}
             >
               <label className="text-gray-400">{action.label}</label>
             </div>
-          )}
+          ))}
         </Row>
       </Row>
       <div className="h-4" />
-      <p className="text-gray-600 max-lines-3">{happiness.comment}</p>
+      <p className="max-lines-3 font-normal text-gray-600">
+        {happiness.comment}
+      </p>
     </Card>
   );
 }
