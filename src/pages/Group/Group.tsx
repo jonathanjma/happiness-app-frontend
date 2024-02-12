99452eba7508462027099130ea6163d693ac83c7
zachseidner1 <zachary.seidner@gmail.com>
2024-02-11 19:27:36 -0500

    Abstract group settings to separate component

--- src/pages/Group/Group.tsx
+++ src/pages/Group/Group.tsx
@@ -1,52 +1,29 @@
-import React, { useState } from "react";
-import toast from "react-hot-toast";
-import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useState } from "react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
-import LeftArrowIcon from "../../assets/arrow_left.svg";
 import GraphIcon from "../../assets/graph.svg";
 import PostIcon from "../../assets/post.svg";
 import SettingsIcon from "../../assets/settings.svg";
 import TableIcon from "../../assets/table.svg";
 import BackButton from "../../components/BackButton";
-import Button from "../../components/Button";
 import HappinessTable from "../../components/HappinessTable";
 import Spinner from "../../components/Spinner";
-import TextField from "../../components/TextField";
-import ToastMessage from "../../components/ToastMessage";
-import Column from "../../components/layout/Column";
+import { TabButton, TabPanel } from "../../components/Tabs";
 import Row from "../../components/layout/Row";
 import { QueryKeys } from "../../constants";
 import { useApi } from "../../contexts/ApiProvider";
 import { Group } from "../../data/models/Group";
 import FeedPanel from "./FeedPanel";
-import { TabButton, TabPanel } from "../../components/Tabs";
+import GroupSettings from "./GroupSettings";
 
 export default function GroupPage() {
   const { groupID } = useParams();
   const { api } = useApi();
-  const queryClient = useQueryClient();
   const { isLoading, data, isError } = useQuery<Group>(
     [QueryKeys.FETCH_GROUP_INFO, groupID],
     () => api.get<Group>("/group/" + groupID).then((res) => res.data),
   );
-
-  // group settings:
   const [showGroupSettings, setShowGroupSettings] = useState(false);
-  const [groupNameText, setGroupNameText] = useState("");
-  const { mutate: saveGroupName, isLoading: putGroupNameLoading } =
-    useMutation<Group>({
-      mutationFn: () =>
-        api
-          .put<Group>(`/group/${data!.id}`, { name: groupNameText })
-          .then((res) => res.data),
-      onSuccess: (newGroup: Group) => {
-        queryClient.setQueryData<Group>(
-          [QueryKeys.FETCH_GROUP_INFO, groupID],
-          (oldGroup) => newGroup,
-        );
-        toast.custom(<ToastMessage message="✅ Group Name Changed" />);
-      },
-    });
 
   if (isLoading) {
     return <Spinner className="mx-8 mt-16" />;
@@ -59,32 +36,10 @@ export default function GroupPage() {
 
   if (showGroupSettings) {
     return (
-      <Column className="mx-8 gap-6 pt-16">
-        <Row
-          className="items-center"
-          onClick={() => {
-            setShowGroupSettings(false);
-          }}
-        >
-          <img src={LeftArrowIcon} className="max-w-[24px]" />
-          <label className="font-normal text-gray-600">Back</label>
-        </Row>
-        <h2>Group Settings</h2>
-        <TextField
-          label="Change Group Name:"
-          hint={data!.name}
-          value={groupNameText}
-          onChangeValue={setGroupNameText}
-          onEnterPressed={saveGroupName}
-          className="md:w-1/2 lg:w-1/3 xl:w-1/4"
-        />
-        <Button
-          label="Save Group Name"
-          onClick={saveGroupName}
-          icon={putGroupNameLoading ? <Spinner variaton="SMALL" /> : undefined}
-          variation="TEXT"
-        />
-      </Column>
+      <GroupSettings
+        group={data!}
+        setShowGroupSettings={setShowGroupSettings}
+      />
     );
   }
 
