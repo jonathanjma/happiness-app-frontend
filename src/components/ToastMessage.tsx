f1dde02be671560f96bc847626b2fa257bfe61c3
zachseidner1 <zachary.seidner@gmail.com>
2024-02-14 00:07:31 -0500

    Add toast shadow

--- src/components/ToastMessage.tsx
+++ src/components/ToastMessage.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 export default function ToastMessage({ message }: { message: string }) {
   return (
-    <div className="flex w-[400px] justify-center rounded-lg bg-light_yellow p-4">
+    <div className="flex w-[400px] justify-center rounded-lg bg-light_yellow p-4 shadow-md1">
       <p className="p-0 font-semibold text-secondary">{message}</p>
     </div>
   );
